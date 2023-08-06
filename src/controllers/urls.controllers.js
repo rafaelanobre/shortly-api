1daacdbfe2d1cbfd1c00922611d74b8b828ed031
rafaelanobre <raafanobre@gmail.com>
2023-08-06 13:02:49 -0300

    :sparkles: Feat: get url by id and open url

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -1,7 +1,7 @@
 import { nanoid } from "nanoid";
 import { db } from "../database/database.connection.js";
 
-export default async function shortenUrl(req,res){
+export async function shortenUrl(req,res){
     const {url} = req.body;
 	try{
         const shortUrl = nanoid(13);
@@ -12,6 +12,33 @@ export default async function shortenUrl(req,res){
         const urlId = resultUrl.rows[0].id;
 
         res.status(201).send({ id: urlId, shortUrl });
+    }catch(err){
+        res.status(500).send(err);
+    }
+}
+
+export async function detailsUrl(req,res){
+    const {id} = req.params;
+
+    try{
+        const { rows: [url]} = await db.query(`SELECT id, "shortUrl", url FROM urls WHERE id=$1`, [id]);
+        if (url == undefined) return res.sendStatus(404);
+
+        res.status(200).send(url);
+    }catch(err){
+        res.status(500).send(err);
+    }
+}
+export async function openUrl(req,res){
+    const {shortUrl} = req.params;
+    try{
+        const { rows: [url]} = await db.query(`SELECT * FROM urls WHERE "shortUrl"=$1`, [shortUrl]);
+        if (url == undefined) return res.sendStatus(404);
+
+        const updatedVisitCount = url.visitCount + 1;
+        await db.query(`UPDATE urls SET "visitCount"=$1 WHERE "shortUrl"=$2`, [updatedVisitCount, shortUrl]);
+
+        res.redirect(url.url);
     }catch(err){
         res.status(500).send(err);
     }
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { urlsSchema } from "../schemas/urlsSchema.js";
-import shortenUrl from "../controllers/urls.controllers.js";
+import {detailsUrl, openUrl, shortenUrl} from "../controllers/urls.controllers.js";
 import validateToken from "../middlewares/validateToken.js";
 
 const urlsRouter = Router();
 
 urlsRouter.post("/urls/shorten", validateSchema(urlsSchema), validateToken, shortenUrl);
+urlsRouter.get("/urls/:id", detailsUrl);
+urlsRouter.get("/urls/open/:shortUrl", openUrl);
 
 export default urlsRouter;
