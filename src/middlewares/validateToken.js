f1b35e698716df82314074436ffbff4a5103ddc7
rafaelanobre <raafanobre@gmail.com>
2023-08-07 15:34:21 -0300

    :bug: Fix: validate token error handle

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,19 +1,16 @@
 import Jwt from "jsonwebtoken";
 
 export default function validateToken(req,res,next){
-    const { authorization } = req.headers;
-    const token = authorization?.replace("Bearer ", "");
+    try{
+        const { authorization } = req.headers;
+        const token = authorization?.replace("Bearer ", "");
 
-    if(!token) res.status(401).send('O token não foi enviado corretamente.');
+        if(!token) res.status(401).send('O token não foi enviado corretamente.');
 
-    Jwt.verify(token,
-        process.env.SECRET_JWT  || "chaveSecreta",
-        (error,decoded) =>{
-            if(error){
-                return res.status(401).send('Token inválido.');
-            }
-            req.userId = decoded.id;
-        }
-    );
-    next();
+        const decoded = Jwt.verify(token, process.env.SECRET_JWT  || "chaveSecreta");
+        req.userId = decoded.id;
+        next();
+    }catch(error){
+        return res.status(401).send({message:'Token inválido.'});
+    }
 }
